04409fe277f4e860eba14771ab314747940148d3
Carlos Oliveros <caoa143@uab.edu.com>
2018-08-20 10:33:02 -0500

    fixed encapsulation

--- OOPGeo/Location.cs
+++ OOPGeo/Location.cs
@@ -8,7 +8,25 @@ namespace OOPGeo
     {
         public string Name { get; set; }
         public string Capital { get; set; }
-
-        public int Population { get; set; }
+        public int population { get; set; }
+        public int Population
+        {
+            get
+            {
+                return population;
+            }
+            set                                 //Encapsulation to prevent negative vlaues for population
+                                                //if user sets pop to <0, it will override to 0.  ..
+            {
+                if (value < 0)
+                {
+                    population = 0;
+                }
+                else
+                {
+                    population = value;
+                }
+            }
+        }
     }
 }
